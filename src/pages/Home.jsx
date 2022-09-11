295160c3b8aea0c79f5bf40db724c61a861023a1
krish-98 <muralikrishnanm98@gmail.com>
2022-09-11 22:27:44 +0530

    created pages directory

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React from "react"
+import audio from "../assets/hip-hop-rock-beats-118000.mp3"
+import audio2 from "../assets/bounce-114024.mp3"
 
 const Home = () => {
   return (
@@ -7,6 +9,20 @@ const Home = () => {
         <h1 className="text-3xl font-bold">
           Bonjour, I'm Murali Krishnan A Front-end Developer.
         </h1>
+
+        <audio controls autoPlay src={audio} type="audio/mpeg">
+          Hi
+        </audio>
+
+        <audio
+          className="bg-red-300"
+          controls
+          autoPlay
+          src={audio2}
+          type="audio/mpeg"
+        >
+          Hi
+        </audio>
       </div>
     </main>
   )
